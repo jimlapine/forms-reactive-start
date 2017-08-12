ae210445f1e9348463e85aa38660332c8c8317fd
jimlapine <jim.lapine@gmail.com>
2017-08-12 19:34:31 -0400

    Lesson updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
+
+  // We must import ReactiveFormsModule in our app.module to use the reactive approach
+  signupForm: FormGroup;
+
+  // Max suggest setting up the form in the ngOnInit life cycle method
+  ngOnInit() {
+    // Max suggests wrapping control names in quotes, so it doest not get mangled during minifiaction
+    this.signupForm = new FormGroup({
+      'username' : new FormControl(null),
+      'email' : new FormControl(null),
+      'gender' : new FormControl('male'),
+    });
+  }
 }
